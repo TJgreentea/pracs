1b34dc854bce68ce49ea670cb43eb31c6c577f89
TJgreentea <161820260+TJgreentea@users.noreply.github.com>
2024-03-02 16:40:13 +1030

    Add files via upload

--- function-3-1.cpp
+++ function-3-1.cpp
@@ -1,17 +1,15 @@
-#include <iostream>
-using namespace std;
-
-// Function signature as per the instructions
 bool is_fanarray(int array[], int n) {
-    // Check if n is less than 1
     if (n < 1) return false;
 
-    // Check if the array is a palindrome and ascending to the middle
     for (int i = 0; i < n / 2; i++) {
-        // Palindrome check
         if (array[i] != array[n - 1 - i]) return false;
-        // Ascending order check
-        if (i > 0 && array[i] < array[i - 1]) return false;
+        // Corrected ascending order check
+        if (i > 0 && array[i] < array[i - 1]) return false; // This line was previously incorrect
+    }
+
+    // Check for non-decreasing order up to the middle
+    for (int i = 0; i < (n / 2) - 1; i++) {
+        if (array[i] > array[i + 1]) return false;
     }
 
     return true;
